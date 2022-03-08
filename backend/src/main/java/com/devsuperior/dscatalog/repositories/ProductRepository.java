d73cbeecbd16d149bb731e416aea114b1bcd9fa2
Marcio Cavalcante <mca19ntl@gmail.com>
2022-03-08 10:28:34 -0300

    Filtrar produtos, DISTINCT, IS NULL

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -10,7 +10,7 @@ import com.devsuperior.dscatalog.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-	@Query("SELECT obj FROM Product obj INNER JOIN obj.categories cats WHERE :category IN cats")
+	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE (:category IS NULL OR :category IN cats)")
 	Page<Product> find(Category category, Pageable pageable);
 
 }
